febd6e002b72c03463b6480587ee3d7c3383ef43
Bichi Kim <bichi@live.co.kr>
2019-01-14 22:21:46 +0900

    Remove unresolved parts
    Remove unresolved parts

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,6 @@ const config = {
    * index.html
    */
   head: {
-    title: "Conduit",
     meta: [
       { charset: "utf-8" },
       {
@@ -17,22 +16,6 @@ const config = {
           "width=device-width, initial-scale=1"
       },
     ],
-    link: [
-      {
-         rel: 'stylesheet',
-         href: '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
-      },
-      {
-         rel: 'stylesheet',
-         href: '//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic',
-      },
-      {
-         rel: 'stylesheet',
-         href: 'https://demo.productionready.io/main.css',
-      },
-    ]
-  },
-
   /**
    * Webpack build
    */
