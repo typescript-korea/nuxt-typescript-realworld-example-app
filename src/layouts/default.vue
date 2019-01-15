e62d2f73adc3feb7dde1fed3011e8b50b2ef3904
gloriajun <pureainu@gmail.com>
2019-01-15 23:54:43 +0900

    feat: import component for define layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div
+    app-header(:site-name="siteName")
+    nuxt
+    app-footer(:site-name="siteName")
+</template>
+
+<script lang="ts">
+import {
+  Component, Vue,
+} from 'nuxt-property-decorator'
+import AppHeader from '../components/appHeader.vue';
+import AppFooter from '../components/appFooter.vue';
+
+@Component({
+  components: {
+    AppHeader,
+    AppFooter,
+  }
+})
+export default class extends Vue {
+  siteName: string = 'conduit'
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
