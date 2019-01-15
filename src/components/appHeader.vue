66f329706c3eab1df6ac9314e49bd26bd5442e61
gloriajun <pureainu@gmail.com>
2019-01-15 23:39:39 +0900

    feat: add app-header component

---
+++ src/components/appHeader.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  nav.navbar.navbar-light
+    .container
+      nuxt-link.navbar-brand(to="/") {{ siteName }}
+      ul.nav.navbar-nav.pull-xs-right
+        li.nav-item
+          nuxt-link.nav-link(to="/") Home
+        li.nav-item
+          nuxt-link.nav-link(to="/post")
+            i.ion-compose &nbsp;New Post
+        li.nav-item
+          nuxt-link.nav-link(to="/settings")
+            i.ion-gear-a &nbsp;Settings
+        li.nav-item
+          nuxt-link.nav-link(to="/register") Sign up
+          
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'nuxt-property-decorator'
+
+@Component({})
+export default class AppHeader extends Vue {
+  @Prop() siteName: String
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
