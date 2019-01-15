1ec854caef6cdcde999e09db664f8914a03d980d
gloriajun <pureainu@gmail.com>
2019-01-15 23:46:49 +0900

    feat: add footer component

---
+++ src/components/appFooter.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  footer
+    .container
+      nuxt-link.logo-font(to="/") {{ siteName }}
+      span.attribution
+        | An interactive learning project from 
+        | <a href="https://thinkster.io">Thinkster</a>.
+        | Code &amp; design licensed under MIT.
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'nuxt-property-decorator'
+
+@Component({})
+export default class AppFooter extends Vue {
+  @Prop() siteName: String
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
