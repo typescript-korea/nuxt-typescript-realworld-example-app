d5963583debe1336f4e4dc847e9f470fdfe01728
Bichi Kim <bichi@live.co.kr>
2019-01-23 16:52:34 +0900

    update layout & title

---
+++ src/layouts/default.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .main
+</template>
+
+<script lang="ts">
+import {capitalize} from 'lodash'
+import {
+  Component, Vue,
+} from 'vue-ts'
+
+@Component
+export default class Layout extends Vue {
+  head() {
+    let title = this.$route.name
+    title = title === 'index' ? 'main' : title
+    return {
+      title: capitalize(title),
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
